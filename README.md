65b406f837a72e34308cbaa729e7e37357deeb19
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2024-07-20 11:38:52 -0400

    final push

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Contact List React App
+
+## Some things to note 
+- When using conditional returns, as in 
+contact ? <> {contact.name} </> : not selected
+waht is checked should be the same as whats being returned
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
+import ContactList from './components/ContactList'
+import { useState } from 'react'
+import SelectedContact from './components/SelectedContact'
+
 
 const App = () => {
 
+  const [selectedContactID, setSelectedContactID] = useState(null)
+
   return (
     <>
+      {
+        selectedContactID ? 
+        <SelectedContact selectedContactID={selectedContactID} setSelectedContactID={setSelectedContactID}/> : 
+        <ContactList setSelectedContactID={setSelectedContactID}/>
+      }
+      
     </>
   )
 }
